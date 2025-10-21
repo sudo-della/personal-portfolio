edbe2ccbf5f2634ed4bf707d9b3075566b5a74eb
sudo-della <daniellamitsh@gmail.com>
2025-10-21 17:37:03 +0200

    makes changes on the design of the page

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwtRoMCzcpyHHf8xNbZTLkfcPyth67SIXlnJPMGjOxkJ_U6bWpUFrM-ZsEgIsg0Zxw/exec';
+        
+        const msg = document.getElementById('msg-success');
+        fetch(scriptURL, {
+            method: 'POST',
+            body: new FormData(form)
+        })
+        .then(response => {
+            console.log('Success!', response);
+            alert('Message sent successfully!');
+            setTimeout(function(){
+                msg.innerHTML = ""},5000)
+                form.reset();
+            })
+        })
+        .catch(error => {
+            console.error('Error!', error.message);
+            alert('Something went wrong. Please try again later.');
+        });
+    });
+
+    const form = document.forms['submit-to-google-sheet'];
+
+    
+
