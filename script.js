3f0c382bfd214934e1ab6007a359c79bfa964acb
sudo-della <daniellamitsh@gmail.com>
2025-10-22 13:40:52 +0200

    makes changes on the design of the page

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ document.querySelectorAll('.menu-links a').forEach(link => {
     });
 });
 
-
-
 function closeMenu() {
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
